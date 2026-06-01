54809c7b5fc4fb4b970e4cb771bb4204acbf544b
João Victor <joaodddev@gmail.com>
2026-06-01 11:56:29 -0300

    feat: create user entity

--- finflow-api/src/main/java/com/finflow/user/User.java
+++ finflow-api/src/main/java/com/finflow/user/User.java
@@ -1,4 +1,26 @@
 package com.finflow.user;
 
+import jakarta.persistence.*;
+import lombok.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+    private String password;
+    private LocalDateTime createdAt;
 }
--- finflow-api/src/main/java/com/finflow/user/UserRepository.java
+++ finflow-api/src/main/java/com/finflow/user/UserRepository.java
@@ -1,4 +1,13 @@
 package com.finflow.user;
 
-public class UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+
+public interface UserRepository
+        extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+
+    boolean existsByEmail(String email);
 }

9a173097897f8dd5ab9cfea19dc896e1c5a7b2fa
João Victor <joaodddev@gmail.com>
2026-06-02 21:50:36 -0300

    feat: add global exception handler

---
+++ finflow-api/src/main/java/com/finflow/common/exception/GlobalExceptionHandler.java
@@ -0,0 +1,28 @@
+package com.finflow.common.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import java.time.LocalDateTime;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<ErrorResponse> handleRuntimeException(
+            RuntimeException ex
+    ) {
+
+    ErrorResponse response =
+    new ErrorResponse(
+            ex.getMessage(),
+            HttpStatus.BAD_REQUEST.value(),
+            LocalDateTime.now()
+    );
+
+    return ResponseEntity
+            .badRequest()
+            .body(response);
+    }
+}
